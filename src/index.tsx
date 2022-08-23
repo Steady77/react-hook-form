86a02db813c7c4453e6a8c907ff2c9949f5394f2
Surov Denis <90111915+Steady77@users.noreply.github.com>
2022-08-23 21:26:48 +0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Form from './form';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(<Form />);
